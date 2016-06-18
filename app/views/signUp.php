6fcd4c8c66f3e127a6ae1725ad59d300ce6399b8
Samuel <scollon@student.42.fr>
2016-06-18 12:52:19 +0200

    javascript first step

--- app/views/signUp.php
+++ app/views/signUp.php
@@ -11,3 +11,24 @@
  	<button type="submit">Sign Up</button>
  </form>
 <section>
+<script>
+
+	function comparePassword() {
+		var pass_1 = document.getElementById('password');
+		var pass_2 = document.getElementById('confirm');
+
+		var v1 = pass_1.value;
+		var v2 = pass_2.value;
+
+		if (v1 && v2) {
+			pass_1.style.border-color = pass_2.style.border-color = (v1 === v2) ? 'green' : 'red';
+		}
+	}
+
+	var pass_1 = document.getElementById('password');
+	var pass_2 = document.getElementById('confirm');
+
+	if (pass_1 && pass_2) {
+		pass_1.onkeyup = pass_2.onkeyup = pass_1.onblur = pass_2.onblur = comparePassword;
+	}
+</script>
